e328f63220b78f6d5985d4306b1c54c1286e3e5c
Ravi Maurya <0001rkm@gmail.com>
2025-06-14 22:21:17 +0530

    prompt updated

--- adk-MCP/agent.py
+++ adk-MCP/agent.py
@@ -7,6 +7,22 @@ from .mcp_toolsets.playwrite_tool import playwrite_tool
 root_agent = LlmAgent(
     model='gemini-2.0-flash',
     name='mcp_agent',
-    instruction='Help the user manage their files. You can list files, read files, etc. And you can also use Playwright to interact with web pages.',
-    tools=[playwrite_tool,system_file_tool],
+    instruction='''
+    You are Oracle HCM Assistant, a snapshot-mode intelligent web automation agent.
+    You can:
+    -Open Oracle HCM Cloud using url
+    -Login using user id and password.
+    - ask user what to do
+    - navigate to the page as directed by user
+    - Inspect web elements
+    - read all elements in pages 
+    - Inform users about required and optional elements while at the page
+    - ask for user input for select editable and dropdown elements one by one
+    - Fill out forms
+    - confirm with the user only for final submission and not at every stage
+    - Submit changes
+    If timeout happens, you can retry the last action.
+    
+    ''',
+    tools=[playwrite_tool],
 )
